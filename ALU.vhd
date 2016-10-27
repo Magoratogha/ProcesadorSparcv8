75f584a5a346379471a35ad6b68f826c1c48e79e
Santiago Gómez Grajales <magoratoga@gmail.com>
2016-10-26 21:36:22 -0500

    Add files via upload

--- ALU.vhd
+++ ALU.vhd
@@ -4,7 +4,8 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
 entity ALU is
-    Port ( operando1 : in  STD_LOGIC_VECTOR (31 downto 0);
+    Port ( c : in  STD_LOGIC;
+			  operando1 : in  STD_LOGIC_VECTOR (31 downto 0);
            operando2 : in  STD_LOGIC_VECTOR (31 downto 0);
            aluOP : in  STD_LOGIC_VECTOR (5 downto 0);
            AluResult : out  STD_LOGIC_VECTOR (31 downto 0));
@@ -33,6 +34,32 @@ begin
 				AluResult <= operando1 and not(operando2);
 			when "000111" => --xnor
 				AluResult <= operando1 xnor operando2;
+				
+			when "001000" => -- ADDcc
+				AluResult <= operando1 + operando2;
+			when "001001" => -- SUBcc
+				AluResult <= operando1 - operando2;
+			when "001010" => -- ADDx
+				AluResult <= operando1 + operando2 + c;
+			when "001011" => -- ADDxcc
+				AluResult <= operando1 + operando2 + c;
+			when "001100" => -- SUBx
+				AluResult <= operando1 - operando2 - c;
+			when "001101" => -- SUBxcc
+				AluResult <= operando1 - operando2 - c;
+			when "001110" => -- ORcc
+				AluResult <= operando1 or operando2;
+			when "001111" => -- ANDcc
+				AluResult <= operando1 and operando2;
+			when "010000" => -- XORcc
+				AluResult <= operando1 xor operando2;
+			when "010001" => -- ANDNcc
+				AluResult <= operando1 and not(operando2);
+			when "010010" => -- ORNcc
+				AluResult <= operando1 or not(operando2);
+			when "010011" => -- XNORcc
+				AluResult <= operando1 xnor operando2;					
+				
 			when others => -- Cae el nop
 				AluResult <= "00000000000000000000000000000000";
 		end case;
